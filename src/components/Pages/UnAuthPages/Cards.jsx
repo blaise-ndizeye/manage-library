d6152c303a5b4b8da2243f832fdccc6f484ed68a
blaise <khalifablaise@gmail.com>
2020-10-17 01:26:39 +0200

    Change management photo

--- src/components/Pages/UnAuthPages/Cards.jsx
+++ src/components/Pages/UnAuthPages/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CardUi from './CardUi'
 import img1 from '../AuthPages/images/bg2.png'
 import img2 from '../AuthPages/images/bg4.jpeg'
-import img3 from '../AuthPages/images/bg3.png'
+import img3 from '../AuthPages/images/bg3.jpg'
 
 const Cards = () => {
     return (
@@ -12,10 +12,10 @@ const Cards = () => {
                     <CardUi imgSrc={img1} title="Lending Books" />
                 </div>
                 <div className="col-md-4 col-sm-12">
-                    <CardUi imgSrc={img2} title="Returning Books" />
+                    <CardUi imgSrc={img3} title="Managing Books" />
                 </div>
                 <div className="col-md-4 col-sm-12">
-                    <CardUi imgSrc={img3} title="Managing Books" />
+                    <CardUi imgSrc={img2} title="Returning Books" />
                 </div>
             </div>
         </div>
