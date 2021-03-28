e840b2f5eac79590788c63cd2b211acad162ffae
Blaise Ndizeye <khalifablaise@gmail.com>
2021-03-29 01:59:38 +0200

    Added root uri

--- src/components/actions/rootURL.js
+++ src/components/actions/rootURL.js
@@ -1,4 +1,4 @@
-//const root = 'https://quiet-river-33450.herokuapp.com'
-const root = 'http://localhost:5000/user'
+const root = 'https://quiet-river-33450.herokuapp.com'
+//const root = 'http://localhost:5000/user'
 
 export default root 
