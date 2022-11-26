5b4fd3faaa448867bdeea40d03d0c9b4b425aa43
Blaise Ndizeye <khalifablaise@gmail.com>
2022-11-26 14:21:16 +0200

    Place the copyright to the bottom and being fixed

--- src/components/auth/LoginModal.jsx
+++ src/components/auth/LoginModal.jsx
@@ -1,130 +1,156 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import {
-    Button,
-    Modal,
-    ModalBody,
-    ModalHeader,
-    Form,
-    FormGroup,
-    Label,
-    Input,
-    Alert
-} from 'reactstrap'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import { GoSignIn } from 'react-icons/go'
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert,
+} from "reactstrap"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { GoSignIn } from "react-icons/go"
 
-import { login } from '../actions/auth/authActions'
-import { clearErrors } from '../actions/errorAction'
-import { NavLink, withRouter } from 'react-router-dom'
-import { networkErrorNotify } from '../notification'
+import { login } from "../actions/auth/authActions"
+import { clearErrors } from "../actions/errorAction"
+import { NavLink, withRouter } from "react-router-dom"
+import { networkErrorNotify } from "../notification"
 
 class LoginModal extends Component {
-    state = {
-        modal: false,
-        email: '',
-        password: '',
-        msg: null,
-        loading: false
-    }
+  state = {
+    modal: false,
+    email: "",
+    password: "",
+    msg: null,
+    loading: false,
+  }
 
-    static propTypes = {
-        isAuthententicated: PropTypes.bool,
-        error: PropTypes.object.isRequired,
-        login: PropTypes.func.isRequired,
-        clearErrors: PropTypes.func.isRequired
-    }
+  static propTypes = {
+    isAuthententicated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+    login: PropTypes.func.isRequired,
+    clearErrors: PropTypes.func.isRequired,
+  }
 
-    componentDidUpdate(prevProps) {
-        const { error, networkError } = this.props
-        if (networkError) return networkErrorNotify()
+  componentDidUpdate(prevProps) {
+    const { error, networkError } = this.props
+    if (networkError) return networkErrorNotify()
 
-        if (error !== prevProps.error) {
-            this.setState({ loading: false })
-            if (error.id === 'LOGIN_FAIL') {
-                this.setState({
-                    msg: error.msg.message
-                })
-            } else {
-                this.setState({
-                    msg: null,
-                    email: '',
-                    password: ''
-                })
-            }
-        }
-    }
-
-    toggle = () => {
-        this.props.clearErrors()
-        this.setState({ loading: false })
+    if (error !== prevProps.error) {
+      this.setState({ loading: false })
+      if (error.id === "LOGIN_FAIL") {
         this.setState({
-            modal: !this.state.modal
+          msg: error.msg.message,
         })
-    }
-
-    onChange = e => {
+      } else {
         this.setState({
-            [e.target.name]: e.target.value
+          msg: null,
+          email: "",
+          password: "",
         })
+      }
     }
+  }
 
-    onSubmit = e => {
-        e.preventDefault()
-        const { email, password } = this.state
-        const user = { email, password }
-        this.setState({ loading: true })
-        this.props.login(user)
-        if (this.props.isAunthenticated) {
-            this.toggle()
-            this.props.history.push('/home')
-        }
+  toggle = () => {
+    this.props.clearErrors()
+    this.setState({ loading: false })
+    this.setState({
+      modal: !this.state.modal,
+    })
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    const { email, password } = this.state
+    const user = { email, password }
+    this.setState({ loading: true })
+    this.props.login(user)
+    if (this.props.isAunthenticated) {
+      this.toggle()
+      this.props.history.push("/home")
     }
+  }
 
-    render() {
-        return (<>
-
-            <NavLink className="nav-link nav-link-light" to="#" onClick={this.toggle} >
-                Login
+  render() {
+    return (
+      <>
+        <NavLink
+          className="nav-link nav-link-light"
+          to="#"
+          onClick={this.toggle}
+        >
+          Login
         </NavLink>
-            <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                <ModalHeader toggle={this.toggle}><GoSignIn /> Login</ModalHeader>
-                <ModalBody>
-                    {this.state.msg ? <Alert color="danger">{this.state.msg}</Alert> : null}
-                    <Form onSubmit={this.onSubmit}>
-                        <FormGroup>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>
+            <GoSignIn /> Login
+          </ModalHeader>
+          <ModalBody>
+            {this.state.msg ? (
+              <Alert color="danger">{this.state.msg}</Alert>
+            ) : null}
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email"
+                  className="mb-3"
+                  onChange={this.onChange}
+                  required
+                />
 
-                            <Label for="email">Email</Label>
-                            <Input type="email"
-                                name="email"
-                                id="email"
-                                placeholder="email"
-                                className="mb-3"
-                                onChange={this.onChange} required />
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  className="mb-3"
+                  onChange={this.onChange}
+                  required
+                />
 
-                            <Label for="password">Password</Label>
-                            <Input type="password"
-                                name="password"
-                                id="password"
-                                placeholder="password"
-                                className="mb-3"
-                                onChange={this.onChange} required />
-
-                            <Button disabled={this.state.loading} color="dark" style={{ marginTop: '2rem' }} block>
-                                {this.state.loading ? 'Loading' : (<><GoSignIn /> Login</>)} 
-                        </Button>
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-            </Modal>
-
-        </>);
-    }
+                <Button
+                  disabled={this.state.loading}
+                  color="dark"
+                  style={{ marginTop: "2rem" }}
+                  block
+                >
+                  {this.state.loading ? (
+                    "Loading..."
+                  ) : (
+                    <>
+                      <GoSignIn /> Login
+                    </>
+                  )}
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </>
+    )
+  }
 }
-const mapStateToProps = state => ({
-    isAunthenticated: state.auth.isAunthenticated,
-    error: state.error,
-    networkError: state.auth.networkError
+const mapStateToProps = (state) => ({
+  isAunthenticated: state.auth.isAunthenticated,
+  error: state.error,
+  networkError: state.auth.networkError,
 })
 
-export default connect(mapStateToProps, { login, clearErrors })(withRouter(LoginModal));
+export default connect(mapStateToProps, { login, clearErrors })(
+  withRouter(LoginModal)
+)
--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Provider } from 'react-redux'
-import store from './store'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./index.css"
+import * as serviceWorker from "./serviceWorker"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Provider } from "react-redux"
+import store from "./store"
 
 ReactDOM.render(
-    <Provider store = {store}>
-      <App />
-      <div className="bg-transparent">
-        {/* <hr color='blue'/> */}
-        <h4 className="display-6 text-center text-white"><span>&#169;</span> Copyright 2020</h4>
-      </div>
-    </Provider>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+    <div
+      style={{
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      {/* <hr color='blue'/> */}
+      <h4 className="display-6 text-center text-white">
+        <span>&#169;</span> Copyright 2020
+      </h4>
+    </div>
+  </Provider>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
